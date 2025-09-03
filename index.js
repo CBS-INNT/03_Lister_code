d57d4b879eb73b6369e5a935d55806cb616731ee
IdaHustedDavidsen <id.digi@cbs.dk>
2025-09-03 18:39:11 +0200

    koden fra sidste år virker

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ import App from './App';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
+// Forklaring på denne fil med andre ord: 
+    // index.js = “start motoren med denne bil”
+    // App.js = “bilen” (alt design, logik, navigation, UI osv.)
+
 registerRootComponent(App);
